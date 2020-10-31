5cc288d9ddcaacb102fd171d0a0919174f085f34
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-10-31 22:11:35 +0100

    Added source code for build-a-simple-graphql-server-with-apollo-and-cdk blogpost

---
+++ lambda/graphql.ts
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    introspection: true,
+});
+
+exports.handler = server.createHandler();
--- lib/graphql-lambda-stack.ts
+++ lib/graphql-lambda-stack.ts
@@ -1,9 +1,20 @@
+import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apiGateway from '@aws-cdk/aws-apigateway';
 
 export class GraphqlLambdaStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const graphqlLambda = new lambda.Function(this, 'graphqlLambda', {
+            code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+            handler: 'graphql.handler',
+            runtime: lambda.Runtime.NODEJS_12_X,
+        });
+
+        new apiGateway.LambdaRestApi(this, 'graphqlEndpoint', {
+            handler: graphqlLambda,
+        });
+    }
 }
